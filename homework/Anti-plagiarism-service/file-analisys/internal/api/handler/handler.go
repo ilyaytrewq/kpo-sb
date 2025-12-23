55e11ab00a7a4fb495949d7d98e3b41333f17dfb
ilyaytrewq <tixonov.ilay.2000@mail.ru>
2025-12-23 22:02:09 +0300

    add defer cancel

--- homework/Anti-plagiarism-service/file-analisys/internal/api/handler/handler.go
+++ homework/Anti-plagiarism-service/file-analisys/internal/api/handler/handler.go
@@ -57,7 +57,9 @@ func NewHandler() (*Handler, error) {
 	if err != nil {
 		return nil, err
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	qdrantClient, err := qdrant.NewClientFromEnv(ctx)
 	if err != nil {
 		return nil, err
